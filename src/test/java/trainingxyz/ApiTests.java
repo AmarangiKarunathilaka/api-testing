64d42987003ad7575715b9edae5a6070585256a3
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2024-01-05 18:26:27 +0530

    ApiTest.java file created

--- src/test/java/trainingxyz/ApiTests.java
+++ src/test/java/trainingxyz/ApiTests.java
@@ -1,20 +1,23 @@
 package trainingxyz;
 
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class ApiTests {
 
+
     @Test
     public void getCategories(){
-        String endpoint = "http://localhost:8888/api_testing/category/read.php";
+        String endpoint = "http://localhost:/api_testing/category/read.php";
         var response  = given().when().get(endpoint).then();
     }
 
+
     @Test
     public void getProduct(){
-        String endpoint = "http://localhost:8888/api_testing/category/read_one.php";
+        String endpoint = "http://localhost:/api_testing/category/read_one.php";
         var response=
                 given().
                         queryParam("id",2).

c1e649240f4ca8bd8f1e82ed20eb7920db99cfd9
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2024-01-05 14:49:18 +0530

    initial commit

---
+++ src/test/java/trainingxyz/ApiTests.java
@@ -0,0 +1,24 @@
+package trainingxyz;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiTests {
+
+    @Test
+    public void getCategories(){
+        String endpoint = "http://localhost:8888/api_testing/category/read.php";
+        var response  = given().when().get(endpoint).then();
+    }
+
+    @Test
+    public void getProduct(){
+        String endpoint = "http://localhost:8888/api_testing/category/read_one.php";
+        var response=
+                given().
+                        queryParam("id");
+    }
+
+
+}
